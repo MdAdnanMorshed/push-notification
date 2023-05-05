4ba01612fcbe2237bcb89ca54c072d6ff66f09e2
MdAdnanMorshed <adnanmorshed118@gmail.com>
2023-05-05 11:25:15 +0600

    first commit

---
+++ android/app/src/main/java/com/notification/app/notification_testing/MainActivity.java
@@ -0,0 +1,6 @@
+package com.notification.app.notification_testing;
+
+import io.flutter.embedding.android.FlutterActivity;
+
+public class MainActivity extends FlutterActivity {
+}
---
+++ lib/push_notification.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class PushNotification extends StatefulWidget {
+  const PushNotification({Key? key}) : super(key: key);
+
+  @override
+  State<PushNotification> createState() => _PushNotificationState();
+}
+
+class _PushNotificationState extends State<PushNotification> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Push Notification'),
+      ),
+    );
+  }
+}
